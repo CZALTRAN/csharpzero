95d775b942c0f27d372a17e2d8c0abe2075bab54
cesar <cesar@email.com.br>
2021-01-16 17:23:39 -0400

    Meu primeiro programa

---
+++ 0001-HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _0001_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
